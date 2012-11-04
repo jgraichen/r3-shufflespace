06c194342006d00d71806581cde7fe72933e9ef0
Jan Graichen <jg@altimos.de>
2012-11-04 11:14:52 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shufflespace::Application.config.secret_token = 'cd4ea49ef90e0aeab364ac7bd00f66cb11bace5e04d40a58bfa45facc66e257c26aeed391f8d56652618aa63072f449062af2d4a4ce44c4909714a767331b02c'
